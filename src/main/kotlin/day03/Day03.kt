ab059f0d44a46211352a8d6a896bd451fd800708
Navid Rojiani <nrojiani@gmail.com>
2025-12-17 13:59:09 -0500

    Day 3

---
+++ src/main/kotlin/day03/Day03.kt
@@ -0,0 +1,52 @@
+package day03
+
+internal typealias Joltage = Int
+
+internal typealias Battery = List<Joltage>
+
+class Day03 {
+
+  class Part1 {
+    fun solve(input: String): Long {
+      val batteries: List<Battery> = parseInput(input)
+      return batteries.sumOf { maxJoltage(it) }
+    }
+
+    private fun maxJoltage(battery: Battery): Long {
+      // First number = the max excluding the last element
+      val (firstDigitIndex, firstDigit) = battery.dropLast(1).withIndex().maxBy { it.value }
+      val secondDigit: Joltage = battery.drop(firstDigitIndex + 1).max()
+      return "${firstDigit}${secondDigit}".toLong()
+    }
+  }
+
+  class Part2 {
+    fun solve(input: String): Long {
+      val batteries: List<Battery> = parseInput(input)
+      return batteries.sumOf { battery -> maxJoltage(battery, numBatteriesToEnable = 12) }
+    }
+
+    /**
+     * Now, you need to make the largest joltage by turning on exactly twelve batteries within each
+     * bank.
+     */
+    private fun maxJoltage(battery: Battery, numBatteriesToEnable: Int): Long {
+      val batteriesToEnable = ArrayDeque<IndexedValue<Joltage>>()
+
+      for (batteryNumber in 1..numBatteriesToEnable) {
+        val max =
+          battery
+            .withIndex()
+            .toList()
+            .drop(batteriesToEnable.lastOrNull()?.index?.plus(1) ?: 0)
+            .dropLast(numBatteriesToEnable - batteryNumber)
+            .maxBy { it.value }
+        batteriesToEnable.addLast(max)
+      }
+      return batteriesToEnable.map { it.value }.joinToString("").toLong()
+    }
+  }
+}
+
+private fun parseInput(input: String): List<Battery> =
+  input.lines().map { line -> line.map { it.digitToInt() } }
