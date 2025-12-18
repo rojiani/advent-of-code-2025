6c264bf04d1e92ea0b1b1ea9af7057e7d4a3e2eb
Navid Rojiani <nrojiani@gmail.com>
2025-12-18 11:03:06 -0500

    Fix Day 2 tests

--- src/test/kotlin/day02/Day02Test.kt
+++ src/test/kotlin/day02/Day02Test.kt
@@ -32,28 +32,6 @@ class Day02Test {
   @Test
   fun `part 2 input`() {
     val input = readInputText("day02-input.txt")
-    part2.solve(input).shouldBe(0)
+    part2.solve(input).shouldBe(47477053982.toBigInteger())
   }
 }
-
-class SampleInputIndividualRangeTest :
-  FunSpec({
-    val part1 = Day02.Part1()
-
-    data class SampleInputTestCase(val input: String, val expectedSum: BigInteger)
-
-    context("part 1 sample input examples") {
-      withData(
-        SampleInputTestCase("11-22", (11 + 22).toBigInteger()),
-        SampleInputTestCase("95-115", 99.toBigInteger()),
-        SampleInputTestCase("998-1012", 1010.toBigInteger()),
-        SampleInputTestCase("1188511880-1188511890", 1188511885.toBigInteger()),
-        SampleInputTestCase("222220-222224", 222222.toBigInteger()),
-        SampleInputTestCase("1698522-1698528", BigInteger.ZERO),
-        SampleInputTestCase("446443-446449", 446446.toBigInteger()),
-        SampleInputTestCase("38593856-38593862", 38593859.toBigInteger()),
-      ) { (input, expected) ->
-        part1.solve(input) shouldBe expected
-      }
-    }
-  })
