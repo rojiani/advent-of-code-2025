bc04eb7ccbb691c93499fbf6ea357a57f0ecf322
Navid Rojiani <nrojiani@gmail.com>
2025-12-18 11:04:26 -0500

    Update Day 3 test

--- src/test/kotlin/day03/Day03Test.kt
+++ src/test/kotlin/day03/Day03Test.kt
@@ -1,10 +1,7 @@
 package day03
 
 import common.readInputText
-import io.kotest.core.spec.style.FunSpec
-import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
-import java.math.BigInteger
 import kotlin.test.Test
 
 class Day03Test {
@@ -35,25 +32,3 @@ class Day03Test {
     part2.solve(input).shouldBe(168798209663590L)
   }
 }
-
-class SampleInputIndividualRangeTest :
-  FunSpec({
-    val part1 = Day03.Part1()
-
-    data class SampleInputTestCase(val input: String, val expectedSum: BigInteger)
-
-    context("part 1 sample input examples") {
-      withData(
-        SampleInputTestCase("11-22", (11 + 22).toBigInteger()),
-        SampleInputTestCase("95-115", 99.toBigInteger()),
-        SampleInputTestCase("998-1012", 1010.toBigInteger()),
-        SampleInputTestCase("1188511880-1188511890", 1188511885.toBigInteger()),
-        SampleInputTestCase("222220-222224", 222222.toBigInteger()),
-        SampleInputTestCase("1698522-1698528", BigInteger.ZERO),
-        SampleInputTestCase("446443-446449", 446446.toBigInteger()),
-        SampleInputTestCase("38593856-38593862", 38593859.toBigInteger()),
-      ) { (input, expected) ->
-        part1.solve(input) shouldBe expected
-      }
-    }
-  })
